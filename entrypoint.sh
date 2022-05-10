6de9fc40ce34e773accd53c85c5802efae710bff
tetneo003 <105028467+tetneo003@users.noreply.github.com>
2022-05-10 13:57:48 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-ngrok config add-authtoken 28QPxo5JqoV6wWKid3GSuD79X7j_4WttKmnxxvcGCy2BF9uEk > /dev/null
-ngrok tcp 3128 &
+ngrok config add-authtoken 28QPxo5JqoV6wWKid3GSuD79X7j_4WttKmnxxvcGCy2BF9uEk
+ngrok tcp 3128 --log=stdout > ngrok.log
 exec /usr/sbin/squid3 -N -d 0 -f /etc/squid/squid.conf
